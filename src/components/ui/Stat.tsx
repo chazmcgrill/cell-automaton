cc06ebd1f85b61605a8af65565c5b98ca413cff9
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-01-07 12:59:15 +0000

    feat: makes stat component use sass instead of styled components

--- src/components/ui/Stat.tsx
+++ src/components/ui/Stat.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 
 interface StatProps {
     label: string;
     value: number;
 }
 
-const StatContainer = styled.div`
-    display: flex;
-    margin: 0 1.5rem;
-    align-items: center;
-`;
-
-const StatLabel = styled.span`
-    margin-right: 0.5rem;
-`;
-
-const StatValue = styled.span`
-    font-size: 1.4rem;
-`;
-
 const Stat = ({ label, value }: StatProps): JSX.Element => (
-    <StatContainer>
-        <StatLabel>{label}</StatLabel>
-        <StatValue>{value}</StatValue>
-    </StatContainer>
+    <div className="stat">
+        <span className="stat-label">{label}</span>
+        <span className="stat-value">{value}</span>
+    </div>
 );
 
 export default Stat;
