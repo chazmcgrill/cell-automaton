f8d733d85c0b7497759b621c528891cd8ee41117
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-01-07 11:56:26 +0000

    feat: makes button component use sass instead of styled components

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,5 @@
 import React, { memo, useCallback } from 'react';
 import { ButtonValue } from './types';
-import styled from 'styled-components';
-import { darken } from 'polished';
 
 type ButtonPriority = 'default' | 'important' | 'primary' | 'danger';
 
@@ -10,44 +8,18 @@ interface ButtonProps<T extends ButtonValue> {
     value?: T;
     onClick: (value?: T) => void;
     disabled?: boolean;
-    priority?: 'default' | 'important' | 'primary' | 'danger';
+    priority?: ButtonPriority;
 }
 
-const StyledButton = styled.button<{ priority: ButtonPriority }>`
-    outline: none;
-    border-style: none;
-    border-radius: 3px;
-    padding: 0.5rem 1rem;
-    margin: 0 0.5rem;
-    transition: background-color 0.3s;
-    cursor: pointer;
-    background-color: ${({ priority, theme }) => priority === 'primary' ? theme.colors.primary : theme.colors.default};
-    color: ${({ priority, theme }) => priority === 'primary' ? theme.colors.white : theme.colors.black};
-
-    &:hover {
-        background-color: ${({ priority, theme }) => darken(0.1, priority === 'primary' ? theme.colors.primary : theme.colors.default)}
-    }
-`;
-
-const Button = <T extends ButtonValue, >({
-    onClick,
-    value,
-    label,
-    disabled,
-    priority = 'default',
-}: ButtonProps<T>): JSX.Element => {
+const Button = <T extends ButtonValue>({ onClick, value, label, disabled, priority = 'default' }: ButtonProps<T>): JSX.Element => {
     const handleClick = useCallback(() => {
-        onClick(value)
+        onClick(value);
     }, [value, onClick]);
 
     return (
-        <StyledButton
-            disabled={disabled}
-            onClick={handleClick}
-            priority={priority}
-        >
+        <button disabled={disabled} onClick={handleClick} className={`button ${priority}`}>
             {label}
-        </StyledButton>
+        </button>
     );
 };
 
